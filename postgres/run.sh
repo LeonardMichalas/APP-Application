1faa53dcc7534c64fa8597bf79a815333f64fab2
Leonard Michalas <leonard.michalas@outlook.com>
2020-02-09 13:21:15 +0100

    Project start. Dev environment and Dockerfiles in place.

---
+++ postgres/run.sh
@@ -0,0 +1,1 @@
+sudo docker run -it --rm -p 5432:5432 -v /home/leo/Development/app/postgres/postgres-data:/var/lib/postgresql/data postgres
---
+++ python/run.sh
@@ -0,0 +1,1 @@
+sudo docker run -it --rm -p 5000:5000 -v /home/leo/Development/app/python/app:/app python
