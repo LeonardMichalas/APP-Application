e0e0661aacc34e011c77b61d1292b6884194abad
Leo <leonard.michalas@outlook.com>
2025-04-19 23:57:03 +0200

    fixed python backend dockerfile

--- python/app/index.py
+++ python/app/index.py
@@ -1,8 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route("/")
+
+@app.route('/')
 def hello():
-    return "Hello Jupiter!"
+    return 'Hello Jupiter!'
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+    app.run(host="0.0.0.0", port=int("8000"), debug=True)
 
